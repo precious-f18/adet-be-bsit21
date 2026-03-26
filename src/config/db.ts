7961a03557132381a9b8409ac4cdba943a7ccc01
Precious Fillalan <pfillalan62732@liceo.edu.ph>
2026-03-26 16:44:05 +0800

    commit

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,5 +2,15 @@ import mysql from 'mysql2/promise';
 
 const dbConfig = {
     host: 'localhost',
-    port: 3306
-}
+    port: 3000,
+    user: 'root',
+    password: 'bolina123',
+    database: 'bsit-21',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+};
+
+const pool = mysql.createPool(dbConfig);
+
+export default pool;
