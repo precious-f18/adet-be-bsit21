60f8bdc65894952f9dfcb7ec02b60d4dd4bf4485
Precious Fillalan <pfillalan62732@liceo.edu.ph>
2026-03-26 18:21:51 +0800

    fix and replace the null response with proper 404 error handling

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,16 @@
 import mysql from 'mysql2/promise';
-import 'dotenv/config';
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || 'bsit-21',
+const dbConfig = {
+    host: 'localhost',
+    port: 3306, 
+    user: 'root',
+    password: 'bolina',
+    database: 'bsitdb21',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-});
+};
+
+const pool = mysql.createPool(dbConfig);
 
 export default pool;
