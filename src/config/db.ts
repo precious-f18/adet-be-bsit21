a29447ea7ece346513aba160c550bfeb5b33ebd9
Precious Fillalan <pfillalan62732@liceo.edu.ph>
2026-03-26 17:12:15 +0800

    commit

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,16 +1,14 @@
 import mysql from 'mysql2/promise';
+import 'dotenv/config';
 
-const dbConfig = {
-    host: 'localhost',
-    port: 3000,
-    user: 'root',
-    password: 'bolina123',
-    database: 'bsit-21',
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || 'bsit-21',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-};
-
-const pool = mysql.createPool(dbConfig);
+});
 
 export default pool;
--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,5 @@ serve({
   fetch: app.fetch,
   port: 3000
 }, (info) => {
-  console.log(`Server is running on http://localhost:3000`)
+  console.log(`Server is running on http://localhost:${info.port}`)
 })
